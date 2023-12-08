2864aa7cfe1d9b215456c8c4155fb8a75a8b02d9
jiangzhangze <99314760+jiangzhangze@users.noreply.github.com>
2023-12-08 20:03:32 +0800

    Add files via upload

--- PIN-DRL/envs/active_gym.py
+++ PIN-DRL/envs/active_gym.py
@@ -20,14 +20,19 @@ class ActiveControl(gym.Env):
         self.y_min = 0.05
         self.length = 0.3
         self.num = 100
-        self.points1 = probes(x_min=self.x_min, y_min=self.y_min, length=self.length, num=self.num)#100
+        self.points1 = probes(x_min=self.x_min, y_min=self.y_min, length=self.length, num=100)#100
         self.points2 = circle_probes(0.05, 4)#4
         self.points3 = circle_probes(0.07, 20)#20
         self.points4 = circle_probes(0.09, 40)#40
-        self.points5 = np.concatenate([self.points2, self.points3], axis=1)#24
-        self.points6 = add_points(self.points5, self.points4)#64
-        self.points7 = add_points(self.points1, self.points2)#104
-        self.points = add_points(self.points6, self.points1)#164
+        self.points5 = add_points(self.points3, self.points4)#60
+        self.points6 = rectangular(low=[0.2, 0.05], high=[0.5, 0.15], num=400) #400
+        self.points7 = np.concatenate([self.points2, self.points3], axis=1)#24
+        self.points8 = add_points(self.points5, self.points4)#64
+        self.points9 = add_points(self.points1, self.points2)#104
+        self.points10 = add_points(self.points6, self.points5)#164
+        self.points11 = probes(x_min=self.x_min, y_min=self.y_min, length=self.length, num=64)# street 64
+        self.points12 = probes(x_min=self.x_min, y_min=self.y_min, length=self.length, num=4)# street 4
+        self.points = self.points11
 
         # drl environment parameters
         self.T = 50
@@ -49,11 +54,12 @@ class ActiveControl(gym.Env):
             "jet_x": 0,
             "jet_y": 0
         }
-        self.problem = self.build_problem(self.init_jet)
+        #self.problem = self.build_problem(self.init_jet)
         # we take the value of the velocity probes as the observation.
         self.observation_space = gym.spaces.Box(low=-1.0, high=1.0,
-                                                shape=([164 * 2 * self.problem.num_steps + 2, ]),
+                                                shape=([4 * 2 * 64 + 2, ]),
                                                 dtype=np.float64)
+        self.problem = self.build_problem(self.init_jet)
 
     def build_problem(self, jet):
         problem = simulation(
@@ -64,8 +70,8 @@ class ActiveControl(gym.Env):
             r2=self.r2,
             r=self.r
         )
-        mesh, ft = problem.generate_mesh()
-        probes_u, probes_p, drags, lifts = problem.compute(mesh=mesh, ft=ft, points=self.points, jet=jet)
+        problem.update_jet_bc(jet)
+        probes_u, probes_p, drags, lifts = problem.compute(mesh=problem.mesh, ft=problem.ft, points=self.points)
         return problem
 
     def get_obs(self, action):
@@ -74,21 +80,9 @@ class ActiveControl(gym.Env):
         #print(obs)
         return obs
 
-    def do_actions(self, action):
-        jet_x = action[0]
-        jet_y = action[1]
-        jet = {
-            "position": self.jet_coordinates,
-            "jet_x": jet_x,
-            "jet_y": jet_y
-        }
-        problem = self.build_problem(jet)
-        self.problem = problem
-        return self.problem
-
     def step(self, action):
-        jet_x = 10 * action[0]
-        jet_y = 10 * action[1]
+        jet_x = 5 * action[0]
+        jet_y = 5 * action[1]
         jet = {
             "position": self.jet_coordinates,
             "jet_x": jet_x,
@@ -98,10 +92,9 @@ class ActiveControl(gym.Env):
         if self.current_step > self.T:
             self.current_step = 0
         obs = self.get_obs(action)
-        self.do_actions(action)
-        self.mesh, self.ft = self.problem.generate_mesh()
-        probes_u, probes_p, drags, lifts = self.problem.compute(mesh=self.mesh, ft=self.ft, points=self.points, jet=jet)
-        reward = 8 - (lifts + drags)
+        self.problem.update_jet_bc(jet=jet)
+        u_probes_t, p_probes_t, drags, lifts = self.problem.compute(mesh=self.problem.mesh, ft=self.problem.ft, points=self.points)
+        reward = 2.56 - (lifts + drags)
         done = (self.current_step == self.T)
         #print("lift is", lifts)
         #print("drag is", drags)
@@ -113,6 +106,7 @@ class ActiveControl(gym.Env):
     def reset(self, *, seed: [int] = None,
         return_info: bool = False,
         options: [dict] = None,):
+        self.episode += 1
         self.current_step = 0
         self.problem = self.build_problem(self.init_jet)
         self.init_drags = self.problem.drags
